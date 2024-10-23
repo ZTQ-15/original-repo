a653ed3442dd85a10baca8f91c7fdaf7dbc11d24
ZTQ-15 <qilun2016@gmail.com>
2024-10-23 20:20:40 +0800

    added file.py

---
+++ file.py
@@ -0,0 +1,9 @@
+#first code
+print("Hello World!")
+#currently the file's status is untracked.
+#To track the file, we need to add it to the repository using the following command:
+#git add file.py
+#After adding the file, we can commit it using the following command:
+#git commit -m "Added file.py"
+#Finally, we can push the changes to the remote repository using the following command:
+#git push origin master
